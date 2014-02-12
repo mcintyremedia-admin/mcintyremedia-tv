f1302315b14b0b3875f87c70f3a31e19a2d2cfe0
admin@mcintyremedia.tv <admin@mcintyremedia.tv@3bdf51c9-cdb1-d171-b435-e7f1a4a9a7fc>
2014-02-12 15:49:14 +0000

    Fix version problems

--- trunk/plugins/service.network.tunnel/default.py
+++ trunk/plugins/service.network.tunnel/default.py
@@ -1,33 +1,31 @@
 
-import xbmcaddon, xbmcgui, sys, os
+import xbmc, xbmcaddon, xbmcgui, sys, os
 
 sys.path.insert(0, os.path.join(os.getcwd(), 'lib'))
 
 __scriptid__ = "service.network.tunnel"
-__addoninfo__ = xbmcaddon.Addon(id)(__scriptid__)
-__addon__ = __addoninfo__["addon"]
-
+__addon__ = xbmcaddon.Addon(__scriptid__)
+__providers__ = ['SmartDNSProxy']
     
 def loginAndWriteDNS():
     providerid     = __addon__.getSetting('provider')
     username       = __addon__.getSetting('username')
     password       = __addon__.getSetting('password')
-    providermodule = map(__import__, [providerid])[0]
-    provider       = getattr(providermodule, providerid)()
+    providername   = __providers__[int(providerid)]
+    providermodule = map(__import__, [providername])[0]
+    provider       = getattr(providermodule, providername)()
+    
+    xbmc.log("Logging into %s with username %s" % (providername, username))
     if provider.login(username, password):
+        xbmc.log("Writing DNS settings")
         provider.writeDNS()
     else:
         dialog = xbmcgui.Dialog()
-        dialog.ok( "%s : login failed" % [providerid])
+        dialog.ok( "%s : login failed" % providerid)
 	
 
 
 if __name__ == "__main__":
-    firsttimerun = __addon__.getSetting('firsttimerun')
-    if firsttimerun == 'true':
-    #Show msgbox and addon settings screen
-        pass
-   
     enabled = __addon__.getSetting('enabled')
     if enabled == 'true':
         loginAndWriteDNS()
