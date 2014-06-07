9d5dd563a420af4437c3b77d6d4d3eb04a9ce843
admin@mcintyremedia.tv <admin@mcintyremedia.tv@3bdf51c9-cdb1-d171-b435-e7f1a4a9a7fc>
2014-06-07 13:48:43 +0000

    Checkin for McIntyreMedia.tv proxy services

--- trunk/plugins/service.network.tunnel/default.py
+++ trunk/plugins/service.network.tunnel/default.py
@@ -1,32 +1,34 @@
+# -*- coding: utf-8 -*-
 
 import xbmc, xbmcaddon, xbmcgui, sys, os
 
+
 sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), 'lib'))
+
 
 __scriptid__ = "service.network.tunnel"
 __addon__ = xbmcaddon.Addon(__scriptid__)
-__providers__ = ['SmartDNSProxy', 'McIntyreMedia', 'Custom']
+__providers__ = ['McIntyreMedia', 'Custom']
     
-def loginAndWriteDNS():
+def authenticateAndWriteDNS():
     providerid     = __addon__.getSetting('provider')
-    username       = __addon__.getSetting('username')
-    password       = __addon__.getSetting('password')
     providername   = __providers__[int(providerid)]
     providermodule = map(__import__, [providername])[0]
-    provider       = getattr(providermodule, providername)()
+    provider       = getattr(providermodule, providername)(__addon__)
     
-    xbmc.log("Logging into %s with username %s" % (providername, username))
-    if provider.login(username, password):
-        xbmc.log("Writing DNS settings")
-        provider.writeDNS()
-    else:
+    try:
+        xbmc.log("Authenticating %s" % (providername))
+        if provider.authenticate():
+            xbmc.log("Writing DNS settings")
+            provider.writeDNS()
+    except Exception as e:
         dialog = xbmcgui.Dialog()
-        dialog.ok( "%s : login failed" % providerid)
-	
-
+        errstrings = str(e).split('|')
+        dialog.ok("McIntyreMedia.tv Tunnel Error", *errstrings)
+        
 
 if __name__ == "__main__":
     enabled = __addon__.getSetting('enabled')
     if enabled == 'true':
-        loginAndWriteDNS()
+        authenticateAndWriteDNS()
       
