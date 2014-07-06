19c8be8f80b74584504d12b589e394e27481b255
admin@mcintyremedia.tv <admin@mcintyremedia.tv@3bdf51c9-cdb1-d171-b435-e7f1a4a9a7fc>
2014-07-06 05:18:53 +0000

    Updated client software to point to prod box with dev failover

--- trunk/plugins/service.network.tunnel/lib/McIntyreMedia.py
+++ trunk/plugins/service.network.tunnel/lib/McIntyreMedia.py
@@ -5,12 +5,12 @@ import xmlrpclib
 from DNSProxy import DNSProxy
     
 class UserState:
-    Authenticated, Unknown, Active, Expired, Error = range(5)
+    Authenticated, Unknown, Active, Expired, IPChangeRequired, Error = range(6)
     
     
 class McIntyreMedia(DNSProxy):
     
-    __AUTH_SERVICE__ = "http://www.mcintyremedia-dev.bitnamiapp.com:8000/"
+    __AUTH_SERVICE__ = "http://54.248.247.62:8000/"
     __USER_SETTING__ = 'username'
 
     def __init__(self, addon):
@@ -27,14 +27,14 @@ class McIntyreMedia(DNSProxy):
         return self._call(self.authproxy.getProxyDetails)
                         
     def _call(self, fn):
-        try:
+        #try:
             return fn(self.getusername())
-        except xmlrpclib.Fault as e:
-            if e.faultCode == UserState.Unknown:
-                raise Exception(self.addon.getLocalizedString(40001) % e.faultString)
-            elif e.faultCode == UserState.Expired:
-                raise Exception(self.addon.getLocalizedString(40002))
-            elif e.faultCode == UserState.Error:
-                raise Exception(e.faultString)
-        except Exception as e:
-                raise Exception("%s|%s" % (self.addon.getLocalizedString(40003), str(e)))                
+        #except xmlrpclib.Fault as e:
+        #    if e.faultCode == UserState.Unknown:
+        #        raise Exception(self.addon.getLocalizedString(40001) % e.faultString)
+        #    elif e.faultCode == UserState.Expired:
+        #        raise Exception(self.addon.getLocalizedString(40002))
+        #    elif e.faultCode == UserState.Error:
+        #        raise Exception(e.faultString)
+        #except Exception as e:
+        #        raise Exception("%s|%s" % (self.addon.getLocalizedString(40003), str(e)))                
